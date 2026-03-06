058bf562393efaec113d79696071a1a9383cf08b
Túlio Aguiar <axeaguiar@hotmail.com>
2026-03-06 19:26:44 +0100

    Introduce Reducer

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useReducer, useState } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 
@@ -9,12 +9,34 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialTaskState);
 
-  useEffect(() => {
-    console.log(state);
-  }, [state]);
+  const [numero, dispatch] = useReducer((state, action) => {
+    console.log(state, action);
+
+    switch (action) {
+      case "INCREMENT":
+        return state + 1;
+      case "DECREMENT":
+        return state - 1;
+      case "RESET":
+        return 0;
+      default:
+        return state;
+    }
+
+    return state;
+  }, 0);
+
+  // useEffect(() => {
+  //   console.log(state);
+  // }, [state]);
+
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      {children}
+      {/* {children} */}
+      <h1>O número é {numero}</h1>
+      <button onClick={() => dispatch("INCREMENT")}>Incrementar</button>
+      <button onClick={() => dispatch("DECREMENT")}>Decrementar</button>
+      <button onClick={() => dispatch("RESET")}>Resetar</button>
     </TaskContext.Provider>
   );
 }
